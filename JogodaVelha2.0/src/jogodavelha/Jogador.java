9c64dd256c18908d8678beb01255ce2b58a26d1e
Ernane <64796733+ErnaneJ@users.noreply.github.com>
2020-12-11 15:24:19 -0300

    Arquivos adicionados via upload. JDV_2.0
    ---Adicionadas novas funcionalidades:---
    > Cadastro dos Jogadores;
    > Contagem do tempo da partida;
    > Histórico de vitórias e derrotas com implementação de um ranking;
    > Possibilidade do salvamento e leitura dos dados por meio de arquivo.

---
+++ JogodaVelha2.0/src/jogodavelha/Jogador.java
@@ -0,0 +1,12 @@
+
+package jogodavelha;
+/**
+ * @author Ernane
+ **/
+import java.io.Serializable;
+
+public class Jogador implements Serializable { 
+    public String nome; 
+    public int vitorias; 
+    public int derrotas;
+}
